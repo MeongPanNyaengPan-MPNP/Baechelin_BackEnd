cbf68b1b14f9924219b7fdc7a8454d6f79a2eab3
JSOI <jsoiworks@gmail.com>
2022-06-27 20:45:42 +0900

    [Rebase] Change project name, and other settings

---
+++ src/main/java/com/mpnp/baechelin/BaechelinApplication.java
@@ -0,0 +1,13 @@
+package com.mpnp.baechelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaechelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaechelinApplication.class, args);
+	}
+
+}
