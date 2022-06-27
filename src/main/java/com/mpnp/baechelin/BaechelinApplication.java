42534d9a66e17d847fded4732977e4207e9d29b2
JSOI <jsoiworks@gmail.com>
2022-06-27 21:26:29 +0900

    [Fix] Initialize Project

--- src/main/java/com/mpnp/baechelin/BaechelinApplication.java
+++ src/main/java/com/mpnp/baechelin/BaechelinApplication.java
@@ -1,11 +1,12 @@
 package com.mpnp.baechelin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BaechelinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BaechelinApplication.class, args);
 	}
