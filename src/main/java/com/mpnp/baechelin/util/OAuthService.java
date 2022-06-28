2b4c5236742d35f5de08e8da7f9ce184c2320350
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-29 02:51:09 +0900

    #6 [Update] 소셜 로그인
    토큰 받아오는 코드 완성

--- src/main/java/com/mpnp/baechelin/util/OAuthService.java
+++ src/main/java/com/mpnp/baechelin/util/OAuthService.java
@@ -29,8 +29,8 @@ public class OAuthService {
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
-            sb.append("1db0c5eb9c2fdd2ba5412540e5d9a37b"); // REST_API_KEY
-            sb.append("&redirect_uri=http://localhost:9000/oauth/kakao"); // 인가코드 받은 redirect_uri
+            sb.append("&client_id=1db0c5eb9c2fdd2ba5412540e5d9a37b"); // REST_API_KEY
+            sb.append("&redirect_uri=http://localhost:9000/user"); // 인가코드 받은 redirect_uri
             sb.append("&code=" + code);
             bw.write(sb.toString());
             bw.flush();
