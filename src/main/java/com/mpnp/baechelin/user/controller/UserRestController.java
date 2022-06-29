56022ed502842a8e30000163c8e7906b4999aad9
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-29 18:23:55 +0900

    #6 [Update] 유저 토큰 받아오는 controller, service구현
    백 자체에서 테스트해볼 수 없어서 고민 중

--- src/main/java/com/mpnp/baechelin/user/controller/UserRestController.java
+++ src/main/java/com/mpnp/baechelin/user/controller/UserRestController.java
@@ -1,14 +1,10 @@
 package com.mpnp.baechelin.user.controller;
 
-import com.mpnp.baechelin.user.domain.User;
 import com.mpnp.baechelin.user.service.UserService;
-import com.mpnp.baechelin.util.OAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("user")
@@ -17,15 +13,10 @@ public class UserRestController {
 
     private final UserService userService;
 
-    private final OAuthService oAuthService;
-
-    @RequestMapping("")
-    public String getTokenTest(@RequestParam String code) {
-        System.out.println("#######" + code);
-
-        String access_token =oAuthService.getKakaoAccessToken(code);
-        System.out.println("###access_token" + access_token);
-
-        return "index";
+    @RequestMapping("/login")
+    public String kakaoLogin(@RequestParam String code) {
+        String accessToken = userService.getKakaoToken(code);
+        System.out.println("accessToke : " + accessToken);
+        return accessToken;
     }
 }
--- src/main/java/com/mpnp/baechelin/user/service/UserService.java
+++ src/main/java/com/mpnp/baechelin/user/service/UserService.java
@@ -1,18 +1,28 @@
 package com.mpnp.baechelin.user.service;
 
-import com.mpnp.baechelin.user.domain.User;
 import com.mpnp.baechelin.user.repository.UserRepository;
+import com.mpnp.baechelin.util.KakaoAPI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final KakaoAPI kakaoAPI;
 
+
+    public String getKakaoToken(String code) {
+        String accessToken = kakaoAPI.getKakaoAccessToken(code);
+
+        HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(accessToken);
+        System.out.println("userInfo : " + userInfo);
+
+        return accessToken;
+    }
 
 
 }
