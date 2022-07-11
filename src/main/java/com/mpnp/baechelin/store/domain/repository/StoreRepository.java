ee90c5de087bd64df860e5ad093ca8b4a9f611ef
JSoi <jsoiworks@gmail.com>
2022-07-11 12:24:26 +0900

    [Refactor] Remove StoreRepository

--- src/main/java/com/mpnp/baechelin/store/domain/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mpnp.baechelin.store.domain.repository;
-
-import com.mpnp.baechelin.store.domain.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Integer> {
-}
