e3f7e2dceddd3390925e58bc15711fe3ae7f57df
Anna-Jin <anna.jin.6848@gmail.com>
2022-07-16 01:59:48 +0900

    #18 [Update] 잘못된 improt 문 수정

--- src/main/java/com/mpnp/baechelin/bookmark/controller/BookmarkController.java
+++ src/main/java/com/mpnp/baechelin/bookmark/controller/BookmarkController.java
@@ -2,7 +2,6 @@ package com.mpnp.baechelin.bookmark.controller;
 
 import com.mpnp.baechelin.bookmark.dto.BookmarkRequestDto;
 import com.mpnp.baechelin.bookmark.service.BookmarkService;
-import com.mpnp.baechelin.oauth.entity.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
