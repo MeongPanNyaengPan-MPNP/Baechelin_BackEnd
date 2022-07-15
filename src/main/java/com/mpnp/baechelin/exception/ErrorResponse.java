e83f41b90301741e317ca2dafabd4e75209b703c
JSoi <jsoiworks@gmail.com>
2022-07-16 07:34:29 +0900

    #33 [Add] 예외처리 틀

---
+++ src/main/java/com/mpnp/baechelin/exception/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.mpnp.baechelin.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ErrorResponse {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status;
+    private final String error;
+    private final String code;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getStatus())
+                        .error(errorCode.getCode())
+                        .code(errorCode.name())
+                        .message(errorCode.getMessage())
+                        .build()
+                );
+    }
+}
