24efe68f7db8efe82291c53931c529ab3f3cda37
JSoi <jsoiworks@gmail.com>
2022-07-16 02:24:44 +0900

    #11 [Fix] 리뷰 DTO 수정

--- src/main/java/com/mpnp/baechelin/review/dto/ReviewMainResponseDto.java
+++ src/main/java/com/mpnp/baechelin/review/dto/ReviewMainResponseDto.java
@@ -2,6 +2,7 @@ package com.mpnp.baechelin.review.dto;
 
 import com.mpnp.baechelin.review.domain.Review;
 import com.mpnp.baechelin.store.domain.Store;
+import com.mpnp.baechelin.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,8 +25,11 @@ public class ReviewMainResponseDto {
     private double point; //별점
     private List<ReviewImageResponseDto> reviewImageUrlList; //리뷰 이미지 사진
     private List<ReviewResponseDto.TagResponseDto> tagList;
-    public ReviewMainResponseDto(Review review) {
-//        this.storeId = store.getId();
+    public ReviewMainResponseDto(Review review, Store store, User user) {
+        this.storeId = store.getId();
+        this.userId = user.getId();
+        this.storeName = store.getName();
+        this.userName = user.getName();
         this.content = review.getContent();
         this.point = review.getPoint();
         this.reviewImageUrlList = review.getReviewImageList()
--- src/main/java/com/mpnp/baechelin/review/repository/ReviewQueryRepository.java
+++ src/main/java/com/mpnp/baechelin/review/repository/ReviewQueryRepository.java
@@ -28,10 +28,13 @@ public class ReviewQueryRepository extends QuerydslRepositorySupport {
     }
 
     public List<Review> findRecentReviews(BigDecimal lat,
-                                         BigDecimal lng,
-                                         int limit) {
-        BigDecimal[] location = QuerydslLocation.getRange(lat, lng, 20);
-        BooleanBuilder builder = QuerydslLocation.locationBuilder(location[0], location[1], location[2], location[3]);
+                                          BigDecimal lng,
+                                          int limit) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (lat != null && lng != null) {
+            BigDecimal[] location = QuerydslLocation.getRange(lat, lng, 20);
+            builder = QuerydslLocation.locationBuilder(location[0], location[1], location[2], location[3]);
+        }
         // 위도 경도에 해당하는 가게를 찾음 -> 해당 댓글을 다 가져옴 -> 내림차순 정렬 -> limit
         // TODO 쿼리문 개선하기
         return queryFactory.selectFrom(review)
