0ff38773f9bcacf2a35df3c10a7d69b32719718b
Anna-Jin <anna.jin.6848@gmail.com>
2022-07-19 01:45:47 +0900

    #26 [Update] 관리자용 유저 등록 업장 조회 구현 중

--- src/main/java/com/mpnp/baechelin/store/dto/userRegisterStore/UserRegisterStoreImgDto.java
+++ src/main/java/com/mpnp/baechelin/store/dto/userRegisterStore/UserRegisterStoreImgDto.java
@@ -1,13 +1,20 @@
 package com.mpnp.baechelin.store.dto.userRegisterStore;
 
+import com.mpnp.baechelin.store.domain.UserRegisterStoreImg;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@NoArgsConstructor
 public class UserRegisterStoreImgDto {
     private String userRegisterStoreImageUrl;
 
     public UserRegisterStoreImgDto(String userRegisterStoreImageUrl) {
         this.userRegisterStoreImageUrl = userRegisterStoreImageUrl;
+    }
+
+    public UserRegisterStoreImgDto(UserRegisterStoreImg userRegisterStoreImg) {
+        this.userRegisterStoreImageUrl = userRegisterStoreImg.getUserRegisterStoreImageUrl();
     }
 }
--- src/main/java/com/mpnp/baechelin/store/repository/UserRegisterStoreRepository.java
+++ src/main/java/com/mpnp/baechelin/store/repository/UserRegisterStoreRepository.java
@@ -1,9 +1,10 @@
 package com.mpnp.baechelin.store.repository;
 
 import com.mpnp.baechelin.store.domain.UserRegisterStore;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRegisterStoreRepository extends JpaRepository<UserRegisterStore, Integer> {
+    Page<UserRegisterStore> findAll(Pageable pageable);
 }
