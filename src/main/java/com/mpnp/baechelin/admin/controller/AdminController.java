5b41bb50834700b0936eed61eff6584f845b8079
Anna-Jin <anna.jin.6848@gmail.com>
2022-07-20 23:25:15 +0900

    #26 [Update] 관리자용 유저 등록 업장 조회
    - 이미지 리스트를 빼고, responseDto의 id 필드 이름 변경 및 생성자에 추가
    - 페이징 추가

--- src/main/java/com/mpnp/baechelin/admin/controller/AdminController.java
+++ src/main/java/com/mpnp/baechelin/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.mpnp.baechelin.admin.controller;
 
 import com.mpnp.baechelin.admin.dto.AdminResponseDto;
 import com.mpnp.baechelin.admin.service.AdminService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -18,6 +19,7 @@ public class AdminController {
 
     private final AdminService adminService;
 
+    @ApiOperation(value = "유저가 등록한 업장을 관리자 페이지에서 조회하는 메소드")
     @GetMapping("/approval")
     public List<AdminResponseDto> getUserRegisterStore() {
         return adminService.getUserRegisterStore();
--- src/main/java/com/mpnp/baechelin/admin/service/AdminService.java
+++ src/main/java/com/mpnp/baechelin/admin/service/AdminService.java
@@ -4,6 +4,8 @@ import com.mpnp.baechelin.admin.dto.AdminResponseDto;
 import com.mpnp.baechelin.store.domain.UserRegisterStore;
 import com.mpnp.baechelin.store.repository.UserRegisterStoreRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,13 +19,16 @@ public class AdminService {
 
     private final UserRegisterStoreRepository userRegisterStoreRepository;
 
+    /**
+     * 유저가 등록한 업장 조회
+     * @return
+     */
     public List<AdminResponseDto> getUserRegisterStore() {
-//        Page<UserRegisterStore> foundUserRegisterStores = userRegisterStoreRepository.findAll(Pageable.ofSize(5));
+        Page<UserRegisterStore> foundUserRegisterStores = userRegisterStoreRepository.findAll(Pageable.ofSize(5));
 
-        List<UserRegisterStore> foundUserRegisterStores = userRegisterStoreRepository.findAll();
         List<AdminResponseDto> result = new ArrayList<>();
 
-        for (UserRegisterStore foundUserRegisterStore : foundUserRegisterStores) {
+        for (UserRegisterStore foundUserRegisterStore : foundUserRegisterStores.getContent()) {
             AdminResponseDto adminResponseDto = new AdminResponseDto(foundUserRegisterStore);
             result.add(adminResponseDto);
         }
