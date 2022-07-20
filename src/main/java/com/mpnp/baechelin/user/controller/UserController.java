c0740443b585017faa157796260bc9cfc79f637b
Anna-Jin <anna.jin.6848@gmail.com>
2022-07-20 16:56:41 +0900

    #36 [Update] 로그아웃 구현 완료
    프론트에게 access token 삭제 해달라고 부탁하기

--- src/main/java/com/mpnp/baechelin/user/controller/UserController.java
+++ src/main/java/com/mpnp/baechelin/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.mpnp.baechelin.user.controller;
 
+import com.mpnp.baechelin.common.SuccessResponse;
 import com.mpnp.baechelin.user.service.UserService;
 import com.mpnp.baechelin.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +22,13 @@ public class UserController {
     private final UserService userService;
 
     @RequestMapping("/logout")
-    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
-        CookieUtil.deleteCookie(request, response, "refresh_token");
-        return new ResponseEntity<>("로그아웃 완료", HttpStatus.OK);
+    public SuccessResponse logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @AuthenticationPrincipal User user
+            ) {
+        userService.logout(request, response, user.getUsername());
+
+        return new SuccessResponse("로그아웃");
     }
 }
