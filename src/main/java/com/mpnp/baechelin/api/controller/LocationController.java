78a1864b29b9cf7a4275c44cda92bdde2b73bade
JSoi <jsoiworks@gmail.com>
2022-07-22 03:42:46 +0900

    #9 [Update] 주소 기반 검색을 통해서 후보 리스트 추출

--- src/main/java/com/mpnp/baechelin/api/controller/LocationController.java
+++ src/main/java/com/mpnp/baechelin/api/controller/LocationController.java
@@ -1,6 +1,8 @@
 package com.mpnp.baechelin.api.controller;
 
 import com.mpnp.baechelin.api.service.LocationService;
+import com.mpnp.baechelin.store.dto.StoreResultDto;
+import com.mpnp.baechelin.store.dto.StoreSingleResultDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,13 +28,18 @@ public class LocationController {
 
     @ApiOperation(value="주소를 위도, 경도로 바꾸는 함수")
     @GetMapping("/convert/address")
-    public Map<String, Object> getGeoByAddress(@RequestParam String address) {
+    public StoreSingleResultDto.LatLong getGeoByAddress(@RequestParam String address) {
         return locationService.convertAddressToGeo(address);
     }
 
     @ApiOperation(value="위도, 경도, 업장명을 통해 업장 정보를 받아오는 함수")
-    @GetMapping("/convert/keyword")
-    public Map<String, Object> getKeywordByGeoAndStoreName(@RequestParam BigDecimal lat, @RequestParam BigDecimal lng, String storeName){
+    @GetMapping("/convert/store-name")
+    public StoreResultDto.StoreResult getKeywordByGeoAndStoreName(@RequestParam BigDecimal lat, @RequestParam BigDecimal lng, String storeName){
         return locationService.convertGeoAndStoreNameToKeyword(String.valueOf(lat), String.valueOf(lng), storeName);
+    }
+    @ApiOperation(value="위도, 경도, 주소를 통해 후보 업장 정보 리스트를 받아오는 함수")
+    @GetMapping("/convert/geo-address")
+    public List<StoreResultDto.StoreResult> getKeywordByGeoAndAddress(@RequestParam BigDecimal lat, @RequestParam BigDecimal lng, @RequestParam String address){
+        return locationService.convertGeoAndAddressToKeyword(String.valueOf(lat), String.valueOf(lng), address);
     }
 }
