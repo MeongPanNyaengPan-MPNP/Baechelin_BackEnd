31956250673291e7683a97285f79a10b5f51ef3f
Anna-Jin <anna.jin.6848@gmail.com>
2022-07-23 17:44:30 +0900

    #18 [Update] 코드 리팩토링
    필요없는 어노테이션 삭제

--- src/main/java/com/mpnp/baechelin/user/repository/UserRepository.java
+++ src/main/java/com/mpnp/baechelin/user/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.mpnp.baechelin.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findBySocialId(String socialId);
 
