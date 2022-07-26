4048a8ab349df5633e1cc6740c71bbe3f13ccfaf
JSoi <jsoiworks@gmail.com>
2022-07-26 17:49:36 +0900

    #41 [Fix] Domain 오류 수정, ErrorCode 추가

---
+++ src/main/java/com/mpnp/baechelin/store/repository/StoreImgRepository.java
@@ -0,0 +1,7 @@
+package com.mpnp.baechelin.store.repository;
+
+import com.mpnp.baechelin.store.domain.StoreImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreImgRepository extends JpaRepository<StoreImage, Integer> {
+}
