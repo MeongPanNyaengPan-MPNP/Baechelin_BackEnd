5c7725c013eda7ef098952b147845f9d15331349
JSoi <jsoiworks@gmail.com>
2022-07-28 02:37:34 +0900

    #18 [Refactor] 이미지 String으로 출력하도록 통일

--- src/main/java/com/mpnp/baechelin/store/dto/StoreImgResponseDto.java
+++ src/main/java/com/mpnp/baechelin/store/dto/StoreImgResponseDto.java
@@ -1,5 +1,6 @@
 package com.mpnp.baechelin.store.dto;
 
+import com.mpnp.baechelin.store.domain.Store;
 import com.mpnp.baechelin.store.domain.StoreImage;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class StoreImgResponseDto {
     private String storeImageUrl;
-    public StoreImgResponseDto(StoreImage storeImage){
-        this.storeImageUrl = storeImage.getStoreImageUrl();
-    }
     public StoreImgResponseDto(String storeImageUrl) {
         this.storeImageUrl = storeImageUrl;
     }
